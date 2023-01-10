1ee17bedde359a7291beaa8d7b259462e2cd84a8
Mau Cabrera <yamilethfontes6@gmail.com>
2023-01-10 11:25:12 -0600

    Subi 002
    Tipos de Datos

---
+++ 23_01_09_002_TiposDedatos/23_01_09_002_TiposDedatos.cpp
@@ -0,0 +1,15 @@
+// 23_01_09_002_TiposDedatos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//Emma
+
+#include <iostream>
+
+int main()
+{
+    int Numero;
+    std::cout << "Dime un numero\n";
+    std::cin >> Numero; 
+    std::cout << " El numero es: " << Numero;
+
+
+
+}
