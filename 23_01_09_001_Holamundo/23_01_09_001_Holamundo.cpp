be130a28f292be75b36d83fa0916455aab6235f5
Mau Cabrera <yamilethfontes6@gmail.com>
2023-01-12 12:02:47 -0600

    018_Emma
    Estructura condicional Ejercicio 1

--- 23_01_09_001_Holamundo/23_01_09_001_Holamundo.cpp
+++ 23_01_09_001_Holamundo/23_01_09_001_Holamundo.cpp
@@ -1,5 +1,5 @@
 // 23_01_09_001_Holamundo.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+//Emma
 
 #include <iostream>
 
@@ -8,12 +8,3 @@ int main()
     std::cout << "Hello World!\n";
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
