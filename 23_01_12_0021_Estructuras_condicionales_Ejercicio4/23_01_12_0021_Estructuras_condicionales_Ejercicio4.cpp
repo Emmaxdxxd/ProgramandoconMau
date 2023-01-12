0aa8076858de5cd1c3c55887db3e18d055197126
Mau Cabrera <yamilethfontes6@gmail.com>
2023-01-12 14:06:27 -0600

    0021_Emma
    Estructuras Condicionales Ejercicio 4

---
+++ 23_01_12_0021_Estructuras_condicionales_Ejercicio4/23_01_12_0021_Estructuras_condicionales_Ejercicio4.cpp
@@ -0,0 +1,28 @@
+// 23_01_12_0021_Estructuras_condicionales_Ejercicio4.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//Emma
+
+#include <iostream>
+
+int main()
+{
+    int n1;
+
+    std::cout << " Dime un numero:\n";
+    std::cin >> n1;
+
+    if (n1 < 0 ) {
+
+        std::cout << " el numero es negativo bro >:c\n " << n1 << std::endl;
+
+    }
+    else {
+
+        std::cout << " el numero es positivo :D\n" << n1 << std::endl;
+
+    }
+       
+        
+
+  
+}
+
