e9af5996c3eae023e1824bfcf9be1cfbc72a33db
Mau Cabrera <yamilethfontes6@gmail.com>
2023-01-12 11:49:31 -0600

    017_Emma
    Estructura Condicionales Ejercicio  1

---
+++ 23_01_11_017_ EstructuraCondicionales Ejercicio 1/23_01_11_017_ EstructuraCondicionales Ejercicio 1.cpp
@@ -0,0 +1,44 @@
+// 23_01_11_017_ EstructuraCondicionales Ejercicio 1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//Emma
+
+#include <iostream>
+
+int main()
+
+
+{
+    int n1, n2;
+    std::cout << " dime 2 numeros:";
+    std::cin >> n1 >> n2;
+
+
+    if (n1 == n2) {
+
+        std::cout << " Ambos numeros son iguales:";
+    }
+    else
+        if (n1 > n2) {
+            std::cout << " El mayor es:" << n1 << std::endl;
+
+
+
+
+
+        }
+        else {
+
+            std::cout << " El mayor es:" << n2 << std::endl;
+        }
+
+
+
+}
+
+
+
+    
+
+
+   
+
+
