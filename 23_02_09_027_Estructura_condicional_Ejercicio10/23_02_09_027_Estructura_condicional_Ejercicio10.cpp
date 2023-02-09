df4ecd3040a48d93bb46932e3bc6dd4d8f98eb0d
Emma <122298295+Emmaxdxxd@users.noreply.github.com>
2023-02-09 14:22:21 -0600

    027_Emma
    Ejercicio 10 Estructuras Condicionales

---
+++ 23_02_09_027_Estructura_condicional_Ejercicio10/23_02_09_027_Estructura_condicional_Ejercicio10.cpp
@@ -0,0 +1,39 @@
+// 23_02_09_027_Estructura_condicional_Ejercicio10.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//Emma
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	int numero;
+
+
+	cout << "Para mostrar el nombre del mes\n";
+	cout << "Dime un numero del 1 al 12\n";
+	cin >> numero;
+
+
+
+
+
+	switch (numero) {
+	case 1: cout << "El mes es Enero"; break;
+	case 2: cout << "El mes es Febrero"; break;
+	case 3: cout << "El mes es Marzo"; break;
+	case 4: cout << "El mes es Abril"; break;
+	case 5: cout << "El mes es Mayo"; break;
+	case 6: cout << "El mes es Junio"; break;
+	case 7: cout << "El mes es Julio"; break;
+	case 8: cout << "El mes es Agosto"; break;
+	case 9: cout << "El mes es Septiembre"; break;
+	case 10: cout << "El mes es Octubre"; break;
+	case 11: cout << "El mes es Noviembre"; break;
+	case 12: cout << "El mes es  Diciembre"; break;
+		
+	}
+
+	
+    
+}
+
