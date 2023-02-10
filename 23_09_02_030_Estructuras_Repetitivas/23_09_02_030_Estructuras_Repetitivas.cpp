c8b36768ebbcab9f11c7f93049f56f9678c18a64
Emma <122298295+Emmaxdxxd@users.noreply.github.com>
2023-02-09 20:30:26 -0600

    030_Emma
    Estructuras Repetitivas While

---
+++ 23_09_02_030_Estructuras_Repetitivas/23_09_02_030_Estructuras_Repetitivas.cpp
@@ -0,0 +1,30 @@
+// 23_09_02_030_Estructuras_Repetitivas.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+
+{
+	int i;
+
+	i = 1;
+
+	while (i <= 10) {
+
+		cout << i << endl;
+		i++;
+
+	}
+
+	_getch();
+	return 0;
+		
+
+
+
+}
+
