48369131d4fc38a63856edf6fdf6bd389024eb90
Emma <122298295+Emmaxdxxd@users.noreply.github.com>
2023-02-11 10:23:59 -0600

    032_Emma
    Sentencia For

---
+++ 23_11_02_032_Estructuras_Repetitivas_SentenciaFor/23_11_02_032_Estructuras_Repetitivas_SentenciaFor.cpp
@@ -0,0 +1,19 @@
+// 23_11_02_032_Estructuras_Repetitivas_SentenciaFor.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+int main()
+
+{
+    for (int i = 10; i >= 1; i--) {
+        cout << i << endl;
+    }
+
+
+    system("pause");
+    return 0;
+}
+
