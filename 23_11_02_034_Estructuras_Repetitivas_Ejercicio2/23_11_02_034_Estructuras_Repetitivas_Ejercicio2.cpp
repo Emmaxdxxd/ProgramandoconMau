a24ea74e337330ecd3de88d0cb0bd67877ca400d
Emma <122298295+Emmaxdxxd@users.noreply.github.com>
2023-02-11 11:49:55 -0600

    034_Emma
    Ciclos o Bucles Ejercicio 2

---
+++ 23_11_02_034_Estructuras_Repetitivas_Ejercicio2/23_11_02_034_Estructuras_Repetitivas_Ejercicio2.cpp
@@ -0,0 +1,32 @@
+// 23_11_02_034_Estructuras_Repetitivas_Ejercicio2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//Emma
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int numero, i = 0;
+    do {
+    cout << "Dime un numero:" << endl;
+    cin >> numero;
+   if (numero > 0); i++;
+
+    } while (numero != 0);
+
+    cout << "\nResultado:\nLa cantidad de valores mayores a cero es:" << i << endl;
+
+    
+
+    system("pause");
+    return 0;
+} 
+
+
+
+     
+    
+
+ 
+
