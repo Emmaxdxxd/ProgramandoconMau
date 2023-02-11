e5bc96a3a9bf81a6f279c19958fe098a75b2aed7
Emma <122298295+Emmaxdxxd@users.noreply.github.com>
2023-02-11 10:11:32 -0600

    031_Emma
    Sentencia Do While

---
+++ 23_09_02_031_Estructuras_Repetitivas_Sentencia_Do_While/23_09_02_031_Estructuras_Repetitivas_Sentencia_Do_While.cpp
@@ -0,0 +1,30 @@
+// 23_09_02_031_Estructuras_Repetitivas_Sentencia_Do_While.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//Emma
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int i;
+
+    i = 10;
+    do{
+
+        cout << i << endl;
+        i--;
+
+    } while (i >= 1);
+
+    
+    system("pause");
+    return 0;
+
+
+   
+
+
+}
+
